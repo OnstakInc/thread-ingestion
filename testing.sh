d71eacac9f703a03464627b4551e55ebca9d64ec
Raheel Anwar <raheelanwar74@gmail.com>
2018-09-11 18:03:35 +0500

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -33,3 +33,39 @@ EOM
 
 systemctl daemon-reload
 systemctl start dos
+
+
+
+cat > /var/testing/fileaccess.sh <<- EOM
+#!/bin/bash
+
+while true
+do
+ 	echo $(date +%b_%d_%Y_%H_%M_%S)
+    date +%b_%d_%Y_%H_%M_%S >> /var/testing/secretfile
+    sleep 6
+done
+
+
+watch -n 6 date +%b_%d_%Y_%H_%M_%S >> /var/testing/secretfile
+EOM
+
+chmod +x /var/testing/fileaccess.sh
+
+cat > /etc/systemd/system/fileaccess.service <<- EOM
+[Unit]
+Description=File Access Service
+After=network.target
+
+[Service]
+ExecStart=/var/testing/fileaccess.sh
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOM
+
+systemctl daemon-reload
+systemctl start fileaccess
+
+systemctl restart tet-sensor
