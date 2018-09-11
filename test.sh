ddd1a45fc70c4e82ee3dfd1c140eff2aed2068ff
Raheel Anwar <raheelanwar74@gmail.com>
2018-09-11 17:28:52 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+systemctl start firewalld
+
+firewall-cmd --zone=public --permanent --add-port=8484/tcp
+firewall-cmd --zone=public --permanent --add-port=8080/tcp
+
+firewall-cmd --reload
+
+
 ipAddress=$(hostname -I)
 
 ping $ipAddress -s 64000 -i 0.1
